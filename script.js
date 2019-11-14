e4b46c40055a57b8af55ba6513e70e99df4033cc
Shane McDonough <shanemcdman@gmail.com>
2019-11-14 13:48:09 -0800

    created most of the javascript. all accept decimal. added on click

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
 console.log("Hello World!");
-let num = new Array(500);//if you ever want to add a posiblity for a longer equation push this number
-let op = new Array(500);
+const Size = 500; //array size.
+let num = new Array(Size);
+let op = new Array(Size);
+let counter = 0; //This keeps track of what sub the op and num array should be
+let resneed = false;
+let dec = false;
+
+window.onload = clear();
+
+function clear()
+{
+	document.getElementById("screentext").innerHTML = "0";
+	for(let i = 0; i < Size; i++)// set every item in op array to null and in num array to 0
+	{
+		num[i] = null;
+		op[i] = null;
+	}//for
+	counter = 0;
+	console.log("CLEAR");
+	dec = false;
+}//clear
+
+function addnum(n)//it adds n to the end of a string
+{
+	if(resneed)
+	{
+		resneed = false;
+		num[counter] = 0;
+	}//if resneed
+	if(num[counter==null])
+	{
+		num[counter] = 0;
+	}
+	if(dec)
+	{
+		num[counter] = num[counter] + n/10;
+	}//if dec
+	else
+		num[counter] = num[counter] * 10 + n;
+	document.getElementById("screentext").innerHTML = num[counter];
+}//addnum
+
+function addop(o)//sets the operator to o and increments the counters
+{
+	op[counter] = o;
+	document.getElementById("screentext").innerHTML = op[counter];
+	counter++;
+	dec = false;
+}//addop
+
+function calculate()
+{
+	let total = num[0];
+	for(let i = 0; i < counter; i++)
+	{
+		if(num[i + 1] != null)
+		{
+			if(op[i] == "+")
+			{
+				total += num[i + 1];
+			}// if +
+			else if(op[i] == "-")
+			{
+				total -= num[i + 1];
+			}// if -
+			else if(op[i] == "*")
+			{
+				total *= num[i + 1];
+			}// if *
+			else if(op[i] == "/")
+			{
+				total /= num[i + 1];
+			}// if /
+		}//if blank
+	}//for i
+	clear();
+	num[0] = total;
+	resneed = true;
+	console.log(total);
+	document.getElementById("screentext").innerHTML = total;
+	return total;
+}//calcualte
+
+function decimal()
+{
+	dec=true;
+	document.getElementById("screentext").innerHTML = num[counter] + ".";
+	console.log("test");
+}//decimal
